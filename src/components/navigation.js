25e22ed90d27c4078bee43acc9f9f8e358486337
zack <zack24q@gmail.com>
2017-08-11 11:29:39 +0800

    update gen key logic

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -33,11 +33,13 @@ export default (keyName) => {
     render() {
       const vnode = this.$slots.default ? this.$slots.default[0] : null
       if (vnode) {
-        const key = getKey(this.$route, keyName)
+        vnode.key = vnode.key || (vnode.isComment
+          ? 'comment'
+          : vnode.tag)
         // prevent vue-router reuse component
-        vnode.key = vnode.key || ''
+        const key = getKey(this.$route, keyName)
         if (vnode.key.indexOf(key) === -1) {
-          vnode.key += key
+          vnode.key = `__navigation-${key}-${vnode.key}`
         }
         if (this.cache[key]) {
           if (vnode.key === this.cache[key].key) {
